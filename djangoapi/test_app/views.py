8021c766e2b98986b0b0d0dbb4a331c92c6881bf
KayBeeDebrah <92883824+KayBeeDebrah@users.noreply.github.com>
2022-08-08 15:18:30 +0100

    Simple class based view with post and get functions that posts and gets all records

--- djangoapi/test_app/views.py
+++ djangoapi/test_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 #import the types of responses u woul work with
 from django.http import JsonResponse, response
 from rest_framework.views import APIView
+#import Model
+from .models import TestModel, RegionModel
+from django.forms.models import model_to_dict
 
 # Create your views here.
 #ClassBased Views
@@ -12,10 +15,18 @@ from rest_framework.views import APIView
 class simple(APIView):
 
     def post(self, request):
-        return JsonResponse({"data": "Data Saved"})
+        new_Region_content = RegionModel.objects.create(
+                 Region_ID=request.data["Region_ID"],
+                 Region_Name=request.data["Region_Name"]
+         )
+        #print(new_Region_content)
+        return JsonResponse({"data": model_to_dict(new_Region_content)})
 
     def get(self, request):
-        return JsonResponse({"data": [3, 2,  1]})
+        #using object manager to filter data from the models
+        #declare variable content to handle data from the model
+        content = RegionModel.objects.all().values()
+        return JsonResponse({"data": list(content)})
 
 
 
